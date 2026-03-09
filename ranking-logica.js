dc7201166c0b628050fa6a9f96da9e826d82ae72
wtfditoh <155072086+wtfditoh@users.noreply.github.com>
2026-03-09 09:55:48 -0300

    Update ranking-logica.js

--- ranking-logica.js
+++ ranking-logica.js
@@ -38,7 +38,7 @@ async function carregarRanking() {
                 lista.push({ 
                     id: doc.id,
                     nome: d.nome, 
-                    xp: number(d.xp) || 0, 
+                    xp: Number(d.xp) || 0, 
                     avatar: d.avatar || "user" 
                 });
             }
